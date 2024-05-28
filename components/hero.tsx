57471af4dcea6f57358ee64c8c12628d8a9d4712
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-05-28 22:07:55 +0800

    initial commit

---
+++ components/hero.tsx
@@ -0,0 +1,10 @@
+
+const Hero = () => {
+  return (
+    <>
+      <img src='hero.png' className="w-screen lg:h-[40rem] mb-10 h-screen lg:rounded-br-full"/>
+    </>
+  )
+}
+
+export default Hero
---
+++ components/products.tsx
@@ -0,0 +1,18 @@
+
+const Products = () => {
+  return (
+    <div className="mt-10 flex flex-col items-center">
+      <h1 className="font-bold text-5xl mb-6">Our Products</h1>
+      <div className="grid grid-cols-2">
+        <img src="p1.png"/>
+        <img src="p2.png"/>
+        <img src="p3.png"/>
+        <img src="p4.png"/>
+        <img src="p5.png"/>
+        <img src="p6.png"/>
+      </div>
+    </div>
+  )
+}
+
+export default Products
