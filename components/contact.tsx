e9ef0bc030eb3e42b697432a3c957d94837ef780
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-05-29 21:40:21 +0800

    adjust contact margins

--- components/contact.tsx
+++ components/contact.tsx
@@ -4,29 +4,7 @@ const Contact = () => {
 
   const currentYear = new Date().getFullYear();
   return (
-  //   <div className="relative bottom-0">
-  //     <div className="relative h-[30rem]">
-  //       <Image
-  //         src="/mfooter.png"
-  //         layout="fill"
-  //         alt="Footer"
-  //       />
-  //     </div>
-  //     <div className="absolute flex flex-col justify-center lg:items-center w-full bottom-20 lg:bottom-32 text-white py-4 px-6">
-  //       <h2 className="text-2xl font-bold mb-2">Contact Us</h2>
-
-  //       <div className="mt-4 flex gap-2"> <img src='loc.svg' width={20}/><p>84 Malakas St. Brgy. Pinyahan Quezon City</p> </div>
-  //       <div className="mt-4 flex gap-2"> <img src='email.svg' width={20}/><p> armand@bradwell.ph</p> </div>
-  //       <div className="mt-4 flex gap-2"> <img src='phone.svg' width={20}/><p> 639 932 1944420</p> </div>
-
-  //     </div>
-  //     <div className="absolute flex justify-center items-center w-full bottom-0 text-white py-4 px-6 gap-4">
-  //       <img src='fb.svg' width={35}/>
-  //       <img src='ig.svg' width={35}/>
-  //     </div>
-  //   </div>
-  
-  <div className="bg-custom-gradient py-12 px-4 sm:px-6 lg:px-8" id="contact">
+    <div className="bg-custom-gradient py-12 px-4 sm:px-6 lg:px-8">
   <div className="max-w-sm mx-auto">
     <h1 className="text-3xl font-bold text-gray-100 mb-4">Contact Us</h1>
     <div className="bg-sky-600 rounded-lg shadow-lg p-6">
@@ -36,12 +14,12 @@ const Contact = () => {
         <p className="text-gray-100">armand@bradwell.ph</p>
       </div>
 
-      <div className="mt-4 flex gap-2 items-start">
+      <div className="mt-3 flex gap-2 items-start">
         <img src='loc.svg' width={20}/>
         <p className="text-gray-100">84 Malakas St. Brgy. Pinyahan Quezon City</p>
       </div>
 
-      <div className="mt-4 flex gap-2 items-start mb-8">
+      <div className="mt-2 flex gap-2 items-start mb-8">
       <img src='phone.svg' width={20}/>
         <p className="text-gray-100">639 932 1944420 <br/>
         (+632) 8295-5835 <br />
@@ -78,7 +56,7 @@ const Contact = () => {
       </div>
     </div>
   </div>
-  <p className="text-white text-center">&copy; {currentYear} Bradwell. All rights reserved.</p>
+  <p className="text-white text-center mt-8">&copy; {currentYear} Bradwell. All rights reserved.</p>
 </div>
 );
 }
