f10af336a308394b717d5b55aab6856eb99b0d6e
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-05-31 23:02:20 +0800

    fix products images

--- components/products.tsx
+++ components/products.tsx
@@ -2,6 +2,12 @@
 
 import { useState, MouseEvent } from "react";
 import Image from "next/image";
+import p1 from "@/assets/images/p1.png"
+import p2 from "@/assets/images/p2.png"
+import p3 from "@/assets/images/p3.png"
+import p4 from "@/assets/images/p4.png"
+import p5 from "@/assets/images/p5.png"
+import p6 from "@/assets/images/p6.png"
 
 const Products = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -23,12 +29,12 @@ const Products = () => {
       <h1 className="font-bold text-5xl">Our Products</h1>
       <p className="lg:hidden">Tap the Product to zoom</p>
       <div className="grid grid-cols-2 gap-4 mt-6">
-        <Image src="/p1.png" width={500} height={500} alt="Product 1" onClick={() => openModal("/p1.png")} priority/>
-        <Image src="/p2.png" width={500} height={500} alt="Product 2" onClick={() => openModal("/p2.png")} priority/>
-        <Image src="/p3.png" width={500} height={500} alt="Product 3" onClick={() => openModal("/p3.png")} priority/>
-        <Image src="/p4.png" width={500} height={500} alt="Product 4" onClick={() => openModal("/p4.png")} priority/>
-        <Image src="/p5.png" width={500} height={500} alt="Product 5" onClick={() => openModal("/p5.png")} priority/>
-        <Image src="/p6.png" width={500} height={500} alt="Product 6" onClick={() => openModal("/p6.png")} priority/>
+        <Image src={p1} width={500} height={500} alt="Product 1" onClick={() => openModal("/p1.png")} priority/>
+        <Image src={p1} width={500} height={500} alt="Product 2" onClick={() => openModal("/p2.png")} priority/>
+        <Image src={p3} width={500} height={500} alt="Product 3" onClick={() => openModal("/p3.png")} priority/>
+        <Image src={p4} width={500} height={500} alt="Product 4" onClick={() => openModal("/p4.png")} priority/>
+        <Image src={p5} width={500} height={500} alt="Product 5" onClick={() => openModal("/p5.png")} priority/>
+        <Image src={p6} width={500} height={500} alt="Product 6" onClick={() => openModal("/p6.png")} priority/>
       </div>
 
       {isOpen && (
