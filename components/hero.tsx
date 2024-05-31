4c003c15ad77da5e6eb38388ceb08eb659c934e7
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-05-31 21:46:36 +0800

    update hero images

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,25 +1,27 @@
 import Image from "next/image";
-
+import MHeroImg from "@/public/hero.png"
+import HeroImg from "@/public/hero2.jpg"
 
 const Hero = () => {
   return (
     <div className="relative h-screen" id="home">
       {/* mobile screen and tablet */}
-      <div className="lg:hidden">
+      <div className="md:hidden">
       <Image
-        src="/hero.png"
-        layout="fill"
+        src={MHeroImg}
         alt="Hero image"
+        className="h-screen w-screen"
+        priority
       />
       </div>
 
       {/* bigger screen */}
-      <div className="hidden lg:block">      
+      <div className="hidden md:block lg:block">      
        <Image
-         src="/hero2.jpg"
-         layout="fill"
-         objectFit="cover"
+         src={HeroImg}
          alt="Hero image1"
+         className="absolute h-screen w-screen"
+         priority
        />
        <div className="absolute h-full w-full bg-white bg-opacity-40">
         <div className="flex flex-col justify-end items-end h-1/2 w-1/2">
